8ba1d01359352f48c99d2fa9ef9ccb0930825221
Cody_Edit <peiyun.jian@superd3d.com>
2015-10-15 19:23:41 +0800

    稀疏自编码: 1. 自实现的L-BFGSS代码,但是其中缺少一步Wolfe条件的线搜索求步长 采用的替代方法是使用固定步长,虽然有时候会出错,但大部分情况是对的.

--- DeepLearning/SparseAutoencode.m
+++ DeepLearning/SparseAutoencode.m
@@ -105,15 +105,16 @@ betrho = beta*(-rho./mrho + (1-rho)./(1-mrho));
 d2 = (W2' * d3 + repmat(betrho,1,patnum) ) .* sigmoidInv(z2);
 
 % 梯度计算
-W1grad = (W1grad + d2*pats') / patnum + lambda*W1;
-b1grad = (b1grad + sum(d2,2)) / patnum;
-W2grad = (W2grad + d3*a2') / patnum + lambda*W2;
-b2grad = (b2grad + sum(d3, 2)) / patnum;
+W1grad = ( d2*pats') / patnum + lambda*W1;
+b1grad = ( sum(d2,2)) / patnum;
+W2grad = ( d3*a2') / patnum + lambda*W2;
+b2grad = ( sum(d3, 2)) / patnum;
 allgrad = [W1grad(:); W2grad(:); b1grad(:); b2grad(:)];
+allWb = [W1(:); W2(:); b1(:); b2(:)];
 
 %% 梯度检查
 % * 检查上述理论梯度值是否符合数值梯度值
-if 1
+if 0
 	eps = 1e-4;
 	nlen = length(allgrad);	
 	len1 = patsize^2 * hidsize^2;
@@ -151,12 +152,140 @@ if 1
 	disp( norm(allgrad-allgradnum) / norm(allgrad+allgradnum) );
 end
 
+
+%% L-BFGS最优化,寻找Wb
+% * 使用minFunc函数
+if 0
+	addpath starter/
+	addpath starter/minFunc
+	options.Method = 'lbfgs';
+	options.maxIter = 100;
+	options.display = 'on';
+	tic;
+	[optWb, cost, ~, ~, lams] = minFunc( @(p) sparseAutoencoderCost(p, patsize^2, hidsize^2, lambda, rho, beta, pats), allWb, options);
+	toc
+end
+
+
+%%
+% * 自实现的L-BFGS算法
+if 1
+	tic;
+	patlen = patsize^2;
+	hidlen = hidsize^2;
+	x = allWb;
+	eps = 1e-4;
+	g = allgrad;
+	d = -g;
+	
+	m = 100;
+	Y = zeros(size(x,1), 0);
+	S = zeros(size(x,1), 0);
+	res = [J];
+	c1 = 1e-4;
+	c2 = 0.9;
+	g_old = g;
+	J_old = J;
+	lam = min(1,1/sum(abs(g)));	
+	for k = 1:200
+		% 缺少一步线搜索步长,目前使用固定步长1
+		% ...
+		%
+		
+		x = x + lam*d;
+		W1 = reshape(x(1:patlen*hidlen), hidlen, []);
+		W2 = reshape(x(patlen*hidlen+1:2*patlen*hidlen), patlen, []);
+		b1 = reshape(x(2*patlen*hidlen+1:2*patlen*hidlen+hidlen), hidlen, []);
+		b2 = reshape(x(2*patlen*hidlen+hidlen+1:end), patlen, []);
+		
+		z2 = W1*pats + repmat(b1, 1, patnum);
+		a2 = sigmoid(z2);
+		z3 = W2*a2 + repmat(b2, 1, patnum);
+		a3 = sigmoid(z3);
+		
+		Jc = sum(sum((a3-pats).^2)) * 0.5 / patnum;
+		Jw = 0.5 * (sum(sum(W1.^2)) + sum(sum(W2.^2)));
+		mrho = sum(a2,2) / patnum;
+		Jsp = sum(rho*log(rho./mrho) + (1-rho)*log((1-rho)./(1-mrho)));
+		J = Jc + lambda*Jw + beta*Jsp;	
+		
+		d3 = -(pats-a3).*sigmoidInv(z3);
+		betrho = beta*(-rho./mrho + (1-rho)./(1-mrho));
+		d2 = (W2' * d3 + repmat(betrho,1,patnum) ) .* sigmoidInv(z2);
+		W1grad = ( d2*pats') / patnum + lambda*W1;
+		b1grad = ( sum(d2,2)) / patnum;
+		W2grad = ( d3*a2') / patnum + lambda*W2;
+		b2grad = ( sum(d3, 2)) / patnum;
+		g = [W1grad(:); W2grad(:); b1grad(:); b2grad(:)];	
+		
+		y = g - g_old;
+		s = lam*d;
+		
+		% 缓存m组		
+		if size(Y,2) < m
+			Y(:,end+1) = y;
+			S(:,end+1) = s;
+		else
+			Y = [Y(:,2:end) y];
+			S = [S(:,2:end) s];
+		end		
+		
+		% two-loop求d_k+1	
+		H0 = (y'*s) / (y'*y);		
+		mcnt = size(Y,2);
+		al = zeros(mcnt, 1);
+		ro = zeros(mcnt, 1);
+		for i = 1:mcnt
+			ro(i) = 1 / (Y(:,i)'*S(:,i));
+		end
+		q = -g;
+		for i = mcnt:-1:1
+			al(i) = ro(i)*(S(:,i)'*q);
+			q = q - al(i)*Y(:,i);
+		end
+		r = H0 * q;
+		for i = 1:mcnt
+			be = ro(i) * (Y(:,i)'*r);
+			r = r + S(:,i)*(al(i) - be);
+		end		
+		
+		d = r;
+		lam = 1;		
+		g_old = g;
+		J_old = J;
+		
+		disp(sprintf('J:%f\t\tit:%d', J, k));
+		res(end+1) = J;
+	end
+	toc
+	clf;
+	plot(res);
+	title('残差-迭代');
+	disp('done');
+	optWb = x;
+end
+
+
+%%
+% * 显示W1信息
+im = zeros(hidsize*(patsize+1)+1, hidsize*(patsize+1)+1);
+W1 = reshape(optWb(1:patsize^2*hidsize^2), hidsize^2, []);
+W1 = W1';
+for i = 1:hidsize^2
+	c = mod((i-1), hidsize)+1;
+	r = floor((i-1) / hidsize)+1;
+	im( (r-1)*(patsize+1)+2:r*(patsize+1), (c-1)*(patsize+1)+2:c*(patsize+1) ) = mat2gray(reshape(W1(:,i), patsize, []));
+end
+clf; imshow(im);
+
 end
 
 function y = sigmoid(x)
-	y = 1 ./ (1 + exp(-x));
+	y = 1 ./ (1+exp(-x));
+	y(isnan(y)) = 0;
 end
 function y = sigmoidInv(x)
-	ex = exp(-x);
+	ex = exp(-x);	
 	y = ex ./ (1+ex).^2;
+	y(isnan(y)) = 0;
 end
